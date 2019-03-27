77dc05a5f5c8931f9daa66eeaa5d221da55e67f9
Mayank Suman <mayanksuman@live.com>
2019-03-27 19:59:46 +0530

    Added support of OpenDocument file diffing

--- install.sh
+++ install.sh
@@ -49,7 +49,7 @@ sudo -E apt install -ym nodejs
 # for markdown and latex
 sudo -E apt-get install -ym pandoc markdown texlive dvipng \
 	texlive-latex-extra texlive-formats-extra texlive-publishers composer \
-	texlive-bibtex-extra biber texlive-font-utils chktex tidy
+	texlive-bibtex-extra biber texlive-font-utils chktex tidy odt2txt
 # for english dictionary
 #sudo -E apt install -ym dictd dict-gcide dict-vera dict-jargon dict-elements \
 #	dict-moby-thesaurus dict
